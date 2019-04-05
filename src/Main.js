a9505e6c4b33d49aa00f4763205bed1e72789432
Matthew McIntyre <mattymac2@gmail.com>
2019-04-05 22:26:24 +0100

    removed redundant css files

--- src/Main.js
+++ src/Main.js
@@ -3,13 +3,17 @@ import DrumMachine from "./containers/DrumMachine";
 import { Provider } from "react-redux";
 import Header from "./components/Header";
 import store from "./store";
-import "./Main.css";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   html {
     background-color: #ed6a5a;
     text-align: center;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
   }
 `;
 
