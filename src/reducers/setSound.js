963774a15050f93fd5c69875cce5123d70a37438
Matthew McIntyre <mattymac2@gmail.com>
2019-04-05 22:26:42 +0100

    redux improvements

--- src/reducers/setSound.js
+++ src/reducers/setSound.js
@@ -2,36 +2,38 @@ const initialState = {
   currentSound: "None"
 };
 
-export default function setSound(state = initialState, action) {
+const setSound = (state = initialState, action) => {
   switch (action.type) {
     case "Q":
-      return { currentSound: "Heater 1" };
+      return { ...state, currentSound: "Heater 1" };
 
     case "W":
-      return { currentSound: "Heater 2" };
+      return { ...state, currentSound: "Heater 2" };
 
     case "E":
-      return { currentSound: "Heater 3" };
+      return { ...state, currentSound: "Heater 3" };
 
     case "A":
-      return { currentSound: "Heater 4" };
+      return { ...state, currentSound: "Heater 4" };
 
     case "S":
-      return { currentSound: "Heater 5" };
+      return { ...state, currentSound: "Heater 5" };
 
     case "D":
-      return { currentSound: "Chord 1" };
+      return { ...state, currentSound: "Chord 1" };
 
     case "Z":
-      return { currentSound: "Chord 2" };
+      return { ...state, currentSound: "Chord 2" };
 
     case "X":
-      return { currentSound: "Chord 3" };
+      return { ...state, currentSound: "Chord 3" };
 
     case "C":
-      return { currentSound: "Kick n Hat" };
+      return { ...state, currentSound: "Kick n Hat" };
 
     default:
       return state;
   }
-}
+};
+
+export default setSound;
